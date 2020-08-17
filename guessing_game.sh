03542d85c2ab30d692120485e8f51b5488c21137
Jhon Jairo Apache Hernandez <jho_ja8@hotmail.com>
2020-08-16 22:09:20 -0500

    updated script, variables was set as integer

--- guessing_game.sh
+++ guessing_game.sh
@@ -3,16 +3,21 @@
 # Simple guessing game
 # Author Jhon Apache
 
-# get a random number < 100
-target=$(($RANDOM % 100))
+# get a random number 0 < target < 100
+declare -ir target=$(( ($RANDOM % 100) + 1 ))
 
 # Initialize the user's guess
-guess=
-until [[ $guess -eq $target ]]; do
+declare -i guess=0
+
+until (( guess == target )); do
     read -p "Take a guess: " guess
-    if [[ $guess -lt $target ]]; then
+
+    # if guess is 0, it was not a number
+    (( guess )) || continue
+
+    if  (( guess < target )); then
         echo "Higher!"
-    elif [[ $guess -gt $target ]]; then
+    elif (( guess > target )); then
         echo "Lower!"
     else
         echo "You found it!!!"
