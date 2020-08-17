de3cef1da22d88ad1c9e9ecc4fc1d8c8faf2f611
Jhon Jairo Apache Hernandez <jho_ja8@hotmail.com>
2020-08-17 12:42:59 -0500

    added documentation

--- change_extension.sh
+++ change_extension.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # change filename extension
+# parameters: the first parameter is the extension that file currently has, the second is the new extension
 # Author: Jhon Apache
 
 [[ $# -ne 2 ]] && { echo "Need exactly two arguments" >&2; exit 1; }
